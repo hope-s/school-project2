6510db876584ae3a585f0b5461545df4c5e42aad
omidprogrammer <72966159+omidprogrammer@users.noreply.github.com>
2021-08-07 11:04:31 +0430

    new ver 2

--- js/scripts.js
+++ js/scripts.js
@@ -416,3 +416,18 @@ function imageZoom(){
 };
 imageZoom();
 // end  of  zoom gallery
+
+
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
